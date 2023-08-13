0adb951de72f12ed69db69c98a76acb1ef579ab8
Đặng Ngọc Quân <dangngocquan104@gmail.com>
2023-08-13 21:13:46 +0700

    Delete old code using sequelize library

--- src/routes/categories.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const categoriesController = require('../controllers/categories');
-
-// GET categories 
-router.use('/', categoriesController.getByPage);
-
-module.exports = router;
