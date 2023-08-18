e84680a40d9f0fe06ba369348cc11b46e62f33c6
Đặng Ngọc Quân <dangngocquan104@gmail.com>
2023-08-19 00:18:59 +0700

    ...

--- src/configs/dbconfig.js
+++ src/configs/dbconfig.js
@@ -1,8 +1,8 @@
 const dbconfig = {
-    host: "localhost",
-    user: "root",
-    password: "12345678",
-    database: "product_manager"
+    host: "dpg-cjfq4gvut75s73a1hmk0-a",
+    user: "dangngocquan",
+    password: "YYkDtsUP4MzwtL24lilEn6JhApt5C6xF",
+    database: "product_manager_rny0"
 }
 
 module.exports = dbconfig;
