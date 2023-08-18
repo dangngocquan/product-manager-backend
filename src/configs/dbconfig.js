3e41a3ef3836c267f454855f542f23257e66108a
Đặng Ngọc Quân <dangngocquan104@gmail.com>
2023-08-19 05:55:21 +0700

    add .env

--- src/configs/dbconfig.js
+++ src/configs/dbconfig.js
@@ -1,8 +1,8 @@
 const dbconfig = {
-    host: "dpg-cjfq4gvut75s73a1hmk0-a",
-    user: "dangngocquan",
-    password: "YYkDtsUP4MzwtL24lilEn6JhApt5C6xF",
-    database: "product_manager_rny0"
+    host: "",
+    user: "",
+    password: "",
+    database: ""
 }
 
 module.exports = dbconfig;
