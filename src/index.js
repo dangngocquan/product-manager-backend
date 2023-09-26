73560838d1bd165bdb6e8b235dbff724cd3648fd
Đặng Ngọc Quân <dangngocquan104@gmail.com>
2023-09-26 09:11:43 +0700

    Update something

--- src/index.js
+++ src/index.js
@@ -23,6 +23,64 @@ app.use(cors({
 
 dotenv.config();
 
+// SESSION
+const session = require('express-session');
+
+app.set('view engine', 'ejs');
+
+app.use(session({
+  resave: false,
+  saveUninitialized: true,
+  secret: 'SECRET' 
+}));
+
+/*  PASSPORT SETUP  */
+
+const passport = require('passport');
+var userProfile;
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.set('view engine', 'ejs');
+
+app.get('/success', (req, res) => res.send(userProfile));
+app.get('/error', (req, res) => res.send("error logging in"));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+/*  Google AUTH  */
+ 
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const GOOGLE_CLIENT_ID = '307003734230-a674ltn77dujprfqmdqd2r370nddmcll.apps.googleusercontent.com';
+const GOOGLE_CLIENT_SECRET = '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';
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+      userProfile=profile;
+      return done(null, userProfile);
+  }
+));
+ 
+app.get('/auth/google', 
+  passport.authenticate('google', { scope : ['profile', 'email'] }));
+ 
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/error' }),
+  function(req, res) {
+    // Successful authentication, redirect success.
+    res.redirect('/success');
+  });
+
 
 const routerCategories = require('./routes/categories');
 const routerAccounts = require('./routes/accounts');
